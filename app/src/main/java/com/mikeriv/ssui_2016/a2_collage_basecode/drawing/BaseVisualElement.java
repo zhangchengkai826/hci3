de83641d5b443bd0c8984dc96819dd4ec0ec5a00
ZhengChengkai826 <zchengkai826@gmail.com>
2019-04-27 10:56:18 +0800

    finish SimpleFrame

---
+++ app/src/main/java/com/mikeriv/ssui_2016/a2_collage_basecode/drawing/BaseVisualElement.java
@@ -0,0 +1,4 @@
+package com.mikeriv.ssui_2016.a2_collage_basecode.drawing;
+
+public class BaseVisualElement extends PrebaseVisualElement {
+}
---
+++ app/src/main/java/com/mikeriv/ssui_2016/a2_collage_basecode/drawing/SimpleFrame.java
@@ -0,0 +1,4 @@
+package com.mikeriv.ssui_2016.a2_collage_basecode.drawing;
+
+public class SimpleFrame extends BaseVisualElement {
+}
